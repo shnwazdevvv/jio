af368fd2f332203ce1c522d64860fe95a7bb5779
shnwazdevvv <shnwaz.devv@gmail.com>
2026-03-24 15:15:42 +0530

    Update text.py

--- jiosaavn/plugins/text.py
+++ jiosaavn/plugins/text.py
@@ -11,7 +11,7 @@ class TEXT:
 - 𝘚𝘦𝘢𝘳𝘤𝘩 𝘧𝘰𝘳 𝘴𝘰𝘯𝘨𝘴, 𝘢𝘭𝘣𝘶𝘮𝘴, 𝘱𝘭𝘢𝘺𝘭𝘪𝘴𝘵𝘴, 𝘢𝘯𝘥 𝘢𝘳𝘵𝘪𝘴𝘵𝘴
 - 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘺𝘰𝘶𝘳 𝘧𝘢𝘷𝘰𝘳𝘪𝘵𝘦 𝘵𝘳𝘢𝘤𝘬𝘴 𝘥𝘪𝘳𝘦𝘤𝘵𝘭𝘺 𝘵𝘰 𝘛𝘦𝘭𝘦𝘨𝘳𝘢𝘮
 - 𝘌𝘹𝘱𝘭𝘰𝘳𝘦 𝘷𝘢𝘳𝘪𝘰𝘶𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦𝘴 𝘵𝘢𝘪𝘭𝘰𝘳𝘦𝘥 𝘵𝘰 𝘦𝘯𝘩𝘢𝘯𝘤𝘦 𝘺𝘰𝘶𝘳 𝘮𝘶𝘴𝘪𝘤 𝘦𝘹𝘱𝘦𝘳𝘪𝘦𝘯𝘤𝘦\n
-**Maintained By: [Anonymous](https://t.me/Ns_AnoNymous)**
+**Maintained By: [Shnwaz](https://t.me/SHNWAZX)**
 """
     HELP_MSG ="""
 **It's very simple to use me! 😉**\n\n
@@ -27,11 +27,11 @@ class TEXT:
 <blockquote>
 **🤖 Bot Name:** [🤖 {me.username}](https://t.me/{me.username})
 **📝 Language:** [Python 3](https://www.python.org/)
-**🧰 Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)
-**👨‍💻 Developer:** [Anonymous](https://t.me/The_proGrammerr)
-**👑 Maintained By:** [RangoZex](https://t.me/RangoZex)
-**📢 Updates Channel:** [NS Bots](https://t.me/NsBotsOfficial)
-**👥 Support Group:** [AMC Support](https://t.me/amcDevSupport)
-**🔗 Source Code:** [GitHub Repository](https://github.com/Ns-AnoNymouS/jiosaavn)
+**🧰 Framework:** [Pyrogram](https://t.me/iwantandroid)
+**👨‍💻 Developer:** [Shnwaz](https://t.me/SHNWAZX)
+**👑 Maintained By:** [RangoZex](https://t.me/iwantandroid)
+**📢 Updates Channel:** [NS Bots](https://t.me/iwantandroid)
+**👥 Support Group:** [AMC Support](https://t.me/iwantandroid)
+**🔗 Source Code:** [GitHub Repository](https://github.com/shnwazdevvv/jio)
 </blockquote>
 """
